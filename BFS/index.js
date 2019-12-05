47f8e4b380bc3089d8d15ac68030c2bf40b5ac20
endiko <kogay.nadezhda@gmail.com>
2019-12-05 11:49:13 +0500

    implementation breadth first search algorithm

---
+++ BFS/index.js
@@ -0,0 +1,28 @@
+const graph = {
+  a: ['b', 'c'],
+  b: ['a', 'd'],
+  c: ['a', 'd'],
+  d: ['b', 'c']
+};
+
+function BFS(graph, targetVertix) {
+  let searchQueue = [];
+  let visited = [];
+
+  searchQueue.push(...graph['a']);
+
+  while (searchQueue.length) {
+    let vertix = searchQueue.shift();
+
+    if (!visited.includes(vertix)) {
+      if (vertix === targetVertix) {
+        return true;
+      }
+      searchQueue.push(...graph[vertix]);
+      visited.push(vertix);
+    }
+  }
+  return false;
+}
+
+console.log(BFS(graph, 'p'));
